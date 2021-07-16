f8a7f705af98d6eede418f69ec75203a6852f522
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2021-07-16 21:28:27 +0300

    link added

--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -5,10 +5,12 @@ import { Route, Switch } from "react-router-dom";
 import Dashboard from "../Pages/Dashboard";
 import Customers from "../Pages/Customers";
 
+import "../styles.css";
+
 const Routes = () => {
   return (
     <Switch>
-      <Route parh="/" component={Dashboard} />
+      <Route path="/" component={Dashboard} />
       <Route path="/customers" component={Customers} />
     </Switch>
   );
