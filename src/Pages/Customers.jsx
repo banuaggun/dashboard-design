2ca60236f71eaf9b1ef73d9d1e1ec559948aa9b5
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2021-08-25 17:13:51 +0300

    customers page arrangement

--- src/Pages/Customers.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Customers = () => {
-  return <div>Customers area</div>;
-};
-
-export default Customers;
---
+++ src/Pages/Customers/Customer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import Table from "../../Components/Tables/Table";
+
+import CustomerList from "../../Assets/Data/CustomerList.json";
+
+import "./customer.css";
+
+const customerHead = [
+  "",
+  "name",
+  "email",
+  "phone",
+  "total orders",
+  "total spend",
+  "location"
+];
+
+const renderHead = (item, index) => <th key={index}>{item}</th>;
+
+const renderBody = (item, index) => (
+  <tr key={index}>
+    <td style={{ border: "1px solid violet", padding: "5px 2px" }}>
+      {item.id}
+    </td>
+    <td>{item.name}</td>
+    <td>{item.email}</td>
+    <td>{item.phone}</td>
+    <td>{item.totalOrders}</td>
+    <td>{item.totalSpend}</td>
+    <td>{item.location}</td>
+  </tr>
+);
+
+const Customers = () => {
+  return (
+    <div>
+      <h2 className="pageHeader">Customers</h2>
+      <div className="row">
+        <div className="col-12 customerTable">
+          <div className="card">
+            <div className="cardBody">
+              <Table
+                limit="10"
+                headData={customerHead}
+                renderHead={(item, index) => renderHead(item, index)}
+                bodyData={CustomerList}
+                renderBody={(item, index) => renderBody(item, index)}
+                className="customerTable"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Customers;
